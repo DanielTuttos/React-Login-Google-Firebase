4c8fed8e639d08922d65ebb602584a2849e6384f
Daniel Romero <tutorialesdanielromero@gmail.com>
2023-03-04 10:32:32 -0500

    adding pages login and home

---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Login = () => {
+  return (
+    <Container>
+      <h1>login</h1>
+    </Container>
+  );
+};
+
+const Container = styled.div``;
