c4d85ef6734c286340244b384daa79f471a47a57
Daniel Romero <tutorialesdanielromero@gmail.com>
2023-04-01 15:54:11 -0500

    agregando contex providr al proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import './App.css';
 import styled from 'styled-components';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Container>
 
---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,44 @@
+import { useContext, createContext, useEffect, useState } from 'react';
+import {
+  GoogleAuthProvider,
+  signInWithRedirect,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth';
+import { auth } from '../api/firebase.config';
+
+const AuthContext = createContext();
+
+export const AurhContextProvider = ({ children }) => {
+  const [user, setUser] = useState({});
+  const googleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithRedirect(auth, provider);
+  };
+  const logOut = () => {
+    signOut(auth);
+  };
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        googleSignIn,
+        logOut,
+        user,
+      }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const UserAuth = () => {
+  return useContext(AuthContext);
+};
